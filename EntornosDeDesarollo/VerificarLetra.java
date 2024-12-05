85efad9a2645f26129b0c47f0740dcbb76a7a211
alemunmar2 <alemunmar2@alu.edu.gva.es>
2024-12-05 15:58:14 +0100

    VerificarLetra terminado

--- EntornosDeDesarollo/VerificarLetra.java
+++ EntornosDeDesarollo/VerificarLetra.java
@@ -1,7 +1,7 @@
 public class VerificarLetra {
     public static boolean verificarLetra(String palabras, char letra, StringBuilder letrasAdivinadas) {
         if (palabras.indexOf(letra) >= 0) {
-            letrasAdivinadas.append(letra);
+            letrasAdivinadas.append(letra) ;
             return letra;
         }
         return palabras;
