409fb916214daef6b8efd74e0949d453f8b6297e
tcanterorodriguez <antcanrod@alu.edu.gva.es>
2024-12-05 15:56:31 +0100

    SeleccionarPalabra finalizado

--- EntornosDeDesarollo/SeleccionarPalabra.java
+++ EntornosDeDesarollo/SeleccionarPalabra.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class SeleccionarPalabra {
     private static final String[] PALABRAS = {"java", "programacion", "ahorcado", "funcion", "juego"};
 
-    public static String seleccionarPalabra() {
+    public static String seleccionarPalabra(){
         Random random = new Random();
         int indice = random.nextInt(PALABRAS.length);
         return PALABRAS[indice];
